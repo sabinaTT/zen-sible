9aa14db4ca6bd8a7d42d7f83d82824c03f3befd1
sabinaTT <tairsbn@gmail.com>
2022-02-20 15:21:16 -0800

    quote shows up in div box but too much info.. need quote only

--- js/script.js
+++ js/script.js
@@ -1,4 +1,92 @@
-/* 
+// $.ajax({
+//     url: "https://api.adviceslip.com/advice/search/{query}"}).then(
+//         (data) => {
+//             // console.log(data);
+//         },
+//         (error) => {
+//             console.log('bad request: ', error)
+//         }
+//     );
+
+$('form').on('submit', (e) => {
+    e.preventDefault();
+    const query = $("input[type='text']").val();
+    const url = `https://api.adviceslip.com/advice/search/${query}`;
+    $.ajax({
+        url
+    }).then(
+        (data) => {
+            $('#quote').html(data)
+        },
+        (error) => {
+            console.log('bad request: ', error)
+        }
+    );
+});
+
+// let btn = document.getElementById("input-button");
+// btn.addEventListener("click", function(e){
+//     e.preventDefault();
+//     console.log("naber")
+// })
+
+// let quotes = document.getElementById("#quotes");
+// const btn = document.getElementById("input-button");
+// const userInput = document.getElementById("input-box");
+// btn.addEventListener("click", function(e) {
+//     e.preventDefault();
+//     let myRequest = new XMLHttpRequest ();
+//     myRequest.open("GET", 'https://api.adviceslip.com/advice/search/`${userInput}`'),
+//     myRequest.onload = function() {
+//         let myData = JSON.parse(myRequest.responseText);
+//         console.log(myData.slips[0].advice)
+//         // renderHTML(myData);
+//     };
+//     myRequest.send(); 
+// });
+
+
+const something = {
+    "total_results": "1",  
+    "query": "book", 
+    "slips": 
+    [
+        {
+            "id": 18,
+            "advice": "Don't judge a book by its cover, unless it has a synopsis on the back.",
+            "date": "2016-04-03"
+        }
+    ]
+}
+// console.log(something.slips[0].advice)
+
+
+// function renderHTML(data) {
+//     let htmlString = "";
+//     quotes.insertAdjacentHTML("beforeend", "testing 123");
+// }
+
+
+
+
+/*
+{
+    "total_results": "1", 
+    "query": "spider", 
+    "slips": [
+        {
+            "id":1,
+            "advice":"Remember that spiders are more afraid of you, than you are of them.",
+            "date":"2015-05-26"
+        }
+    ]
+}
+
+*/
+
+
+
+/*
 const api_url ="https://api.adviceslip.com/advice/search/{query}";
 
 async function getapi(url)
@@ -11,9 +99,7 @@ async function getapi(url)
 getapi(api_url);
 
 
-
-
- 
+/*
 $("form").on("submit", (event) => {
 event.preventDefault();
 const userInput = $('input[type="text"]').val();
@@ -32,7 +118,7 @@ $.ajax({
 })
 */
 
-/* didn't work */
+/* didn't work 
 async function getAdvice(e) {
     e.preventDefault();
 
@@ -50,8 +136,13 @@ async function getAdvice(e) {
 
 $("form").on("submit", getAdvice);
 
-console.log(getAdvice)
+const adviceButton = document.querySelector("#input-button");
+adviceButton.addEventListener("click", function(e){
+
+})
+*/
 
 // $.getJSON("https://api.adviceslip.com/advice/search/{query}", function(callback) {
 //     console.log(callback);
 // });
+
