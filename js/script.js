d7d0f47fb92ec55cd6b12f49c313bed5eded8adb
sabinaTT <tairsbn@gmail.com>
2022-02-26 07:28:17 -0800

    make advice show up in box

--- js/script.js
+++ js/script.js
@@ -3,10 +3,23 @@ $('form').on('submit', (e) => {
     const query = $("input[type='text']").val();
     const url = `https://api.adviceslip.com/advice/search/${query}`;
     $.ajax({
-        url
+        url, 
+        dataType: "JSON"
     }).then(
         (data) => {
-            $('#quote').html(data)
+            // console.log(data)
+            // console.log(data.total_results)
+            /*
+            const adviceArray = data.slips[0].advice;
+            console.log(adviceArray)
+            for (let i = 0; i< adviceArray; i++) {
+                let randomAdvice = adviceArray[Math.floor(Math.random()*adviceArray.length)];
+                console.log(randomAdvice)
+                $('#quote').html(randomAdvice)
+            }
+            */
+            $('#quote').html(data.slips[0].advice[1])
+
         },
         (error) => {
             console.log('bad request: ', error)
